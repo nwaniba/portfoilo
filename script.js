2df705cdc8013d9e530d15788fbf22e64a95f9b4
samuel <daniaomokhafe1@gmail.com>
2023-10-03 22:29:28 +0100

    project

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-// When the user clicks on div, open the popup
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+let diff = 0;
+const txt = ["Web Designer","freelancer", "Developer"];
+const speed = 50;
+let interv;
+
+function typeWriter() {
+  const dis = document.getElementById("oneName");
+  if (diff < txt[0].length) {
+    dis.innerHTML += txt[0].charAt(diff);
+    diff++;
+  } else if (dis.innerHTML.length > 0) {
+    dis.innerHTML = dis.innerHTML.slice(0, -1);
+  } else {
+    clearInterval(interv);
+    txt.push(txt.shift());
+    diff = 0;
+    setTimeout(startTyping, 1000);
   }
+}
+
+function startTyping() {
+  interv = setInterval(typeWriter, 200);
+}
+
+startTyping();
+
+
+
+
